9bd9907762fce95e887b644f180a3351799c5307
elijah <me@elia.li>
2024-07-06 16:44:00 +0200

    cleanup

--- sql/foreign_keys.sql
+++ sql/foreign_keys.sql
@@ -3,23 +3,19 @@ ALTER TABLE tbl_buchung
 ADD CONSTRAINT fk_person_buchung
 FOREIGN KEY (Person_FS)
 REFERENCES tbl_person (Person_ID);
-
 ALTER TABLE tbl_buchung
 ADD CONSTRAINT fk_land_buchung
 FOREIGN KEY (Land_FS)
 REFERENCES tbl_land (Land_ID);
-
 -- Foreign Keys tbl_position
 ALTER TABLE tbl_position
 ADD CONSTRAINT fk_buchung_position
 FOREIGN KEY (Buchung_FS)
 REFERENCES tbl_buchung (Buchung_ID);
-
 ALTER TABLE tbl_position
 ADD CONSTRAINT fk_benutzer_position
 FOREIGN KEY (Benutzer_FS)
 REFERENCES tbl_benutzer (Benutzer_ID);
-
 ALTER TABLE tbl_position
 ADD CONSTRAINT fk_leistung_position
 FOREIGN KEY (Leistung_FS)
