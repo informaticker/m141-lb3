03d393f8b10eecf2c5560fa1af848e8a4e17bfad
elijah <me@elia.li>
2024-07-06 18:59:14 +0200

    Update Readme

--- sql/rename_tables.sql
+++ sql/rename_tables.sql
@@ -1,8 +1,8 @@
--- rename tables
+-- Tabellen umbenennen
 RENAME TABLE tbl_personen TO tbl_person;
 RENAME TABLE tbl_positionen TO tbl_position;
 
--- rename attributes
+-- Attribute umbenennen
 ALTER TABLE tbl_buchung CHANGE COLUMN Buchungs_ID Buchung_ID INT;
 ALTER TABLE tbl_buchung CHANGE COLUMN Personen_FS Person_FS INT;
 ALTER TABLE tbl_person CHANGE COLUMN Personen_ID Person_ID INT;
--- sql/user_perms.sql
+++ sql/user_perms.sql
@@ -3,29 +3,29 @@ CREATE USER IF NOT EXISTS 'benutzer'@'localhost' IDENTIFIED BY 'benutzer_passwor
 CREATE USER IF NOT EXISTS 'management'@'localhost' IDENTIFIED BY 'management_password';
 
 -- Privilegien für Benutzer
-GRANT SELECT, UPDATE ON lb3eliamilena.tbl_person TO 'benutzer'@'localhost';
+GRANT SELECT, UPDATE ON tbl_person TO 'benutzer'@'localhost';
 
 -- tbl_benutzer
 GRANT SELECT, INSERT, UPDATE (Benutzer_ID, Benutzername, Vorname, Name, Benutzergruppe, erfasst, aktiv) 
-ON lb3eliamilena.tbl_benutzer TO 'benutzer'@'localhost';
-GRANT SELECT (deaktiviert) ON lb3eliamilena.tbl_benutzer TO 'benutzer'@'localhost';
+ON tbl_benutzer TO 'benutzer'@'localhost';
+GRANT SELECT (deaktiviert) ON tbl_benutzer TO 'benutzer'@'localhost';
 
 -- tbl_buchung, tbl_position
-GRANT SELECT, INSERT, UPDATE, DELETE ON lb3eliamilena.tbl_buchung TO 'benutzer'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON lb3eliamilena.tbl_position TO 'benutzer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON tbl_buchung TO 'benutzer'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON tbl_position TO 'benutzer'@'localhost';
 
 -- tbl_land, tbl_leistung
-GRANT SELECT ON lb3eliamilena.tbl_land TO 'benutzer'@'localhost';
-GRANT SELECT ON lb3eliamilena.tbl_leistung TO 'benutzer'@'localhost';
+GRANT SELECT ON tbl_land TO 'benutzer'@'localhost';
+GRANT SELECT ON tbl_leistung TO 'benutzer'@'localhost';
 
 -- Privilegien für Management
-GRANT SELECT ON lb3eliamilena.tbl_position TO 'management'@'localhost';
-GRANT SELECT ON lb3eliamilena.tbl_buchung TO 'management'@'localhost';
+GRANT SELECT ON tbl_position TO 'management'@'localhost';
+GRANT SELECT ON tbl_buchung TO 'management'@'localhost';
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON lb3eliamilena.tbl_person TO 'management'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON lb3eliamilena.tbl_benutzer TO 'management'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON lb3eliamilena.tbl_land TO 'management'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON lb3eliamilena.tbl_leistung TO 'management'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON tbl_person TO 'management'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON tbl_benutzer TO 'management'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON tbl_land TO 'management'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON tbl_leistung TO 'management'@'localhost';
 
 -- Privilegien neu laden
 FLUSH PRIVILEGES;
