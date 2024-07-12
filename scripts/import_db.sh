6895effab7caa46cd4bc06debbd59676bfe74958
elijah <me@elia.li>
2024-07-12 09:10:55 +0200

    Rename build scripts

--- scripts/import_db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir import_tmp && cd import_tmp
-wget https://gitlab.com/ch-tbz-it/Stud/m141/m141/-/raw/main/LB3-Praxisarbeit/backpacker_ddl_lb3.sql?ref_type=heads -O backpacker_ddl_lb3.sql
-mysql -p -e "CREATE DATABASE lb3eliamilena;"
-mysql -p lb3eliamilena < backpacker_ddl_lb3.sql
-cd .. && rm -r import_tmp
